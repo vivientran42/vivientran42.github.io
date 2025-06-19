48a5331c05bae1783e80b8e36787eb34d55ae2ca
Vivien Tran <vivitr9@gmail.com>
2025-06-20 01:10:43 +1000

    add modal component

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,23 +1,25 @@
+import { ReactNode } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
 export default function Modal({
-  heading,
-  description,
-  tech,
+  isOpen,
+  onClose,
+  children,
 }: {
-  heading: string;
-  description: string;
-  tech: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
 }) {
+  if (!isOpen) return null;
+
   return (
-    <div className="w-full h-full col-span-1 border border-slate-600 p-10 items-center hover:bg-white hover:bg-opacity-25 hover:shadow-lg">
-      <h1 className="text-slate-600 text-left text-xl leading-relaxed">
-        {heading}
-      </h1>
-      <h1 className="text-slate-600 text-left text-md leading-relaxed mb-4">
-        {description}
-      </h1>
-      <h1 className="text-slate-600 text-left text-xs leading-relaxed">
-        {tech}
-      </h1>
+    <div className="fixed inset-0 bg-slate-950 bg-opacity-40 flex items-center justify-center z-50">
+      <div className="relative bg-stone-50 rounded-xl shadow-lg px-10 py-10 max-w-xl w-full">
+        <button onClick={onClose} className="absolute top-4 right-4">
+          <XMarkIcon className="size-4 text-slate-600 hover:text-pink-500 hover:scale-125 transition duration-150" />
+        </button>
+        {children}
+      </div>
     </div>
   );
 }
